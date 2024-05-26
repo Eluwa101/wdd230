ee232c893ee21b89a4c5af8837d474cb6e45512f
eluwa101 <eluwamonday2017@gmail.com>
2024-05-26 23:54:44 +0100

    update links

--- scripts/links.js
+++ scripts/links.js
@@ -1,8 +1,7 @@
 // Define baseURL and linksURL variables
 const baseURL = 'https://eluwa101.github.io/wdd230/';
-const linksURL = 'https://eluwa101.github.io/wdd230/links.json';
+const linksURL = 'https://eluwa101.github.io/wdd230/scripts/links.json';
 
-// Define an asynchronous function to fetch links data
 async function getLinks() {
   try {
     const response = await fetch(linksURL);
@@ -10,14 +9,13 @@ async function getLinks() {
       throw new Error('Network response was not ok.');
     }
     const data = await response.json();
-    console.log(data); // Test JSON result (remove this line once testing is done)
-    displayLinks(data.weeks); // Call displayLinks function with data as argument
+    console.log(data); 
+    displayLinks(data.weeks);
   } catch (error) {
     console.error('Error:', error);
   }
 }
 
-// Call the getLinks function to start fetching data
 getLinks();
 
 // Define displayLinks function to build activity links
@@ -26,13 +24,16 @@ function displayLinks(weeks) {
   weeks.forEach((weekObj) => {
     const weekItem = document.createElement('li');
     const weekLink = document.createElement('a');
-    weekLink.href = '#'; // Placeholder link
+    weekLink.href = '#';
+
     weekLink.textContent = `${weekObj.week}:`;
     weekItem.appendChild(weekLink);
+
     weekObj.links.forEach((linkObj) => {
       const activityLink = document.createElement('a');
       activityLink.href = baseURL + linkObj.url;
       activityLink.textContent = linkObj.title;
+      
       weekItem.appendChild(activityLink);
       weekItem.appendChild(document.createTextNode('|'));
     });
