edb33e96bb540ff16b40b57c6bd86473abf885c3
eluwa101 <eluwamonday2017@gmail.com>
2024-06-01 03:09:29 +0100

    added all approved changes

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -49,3 +49,43 @@ document.addEventListener('DOMContentLoaded', () => {
     const now = new Date();
     timestamp.value = now.toISOString();
 });
+
+
+
+// directory
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const gridButton = document.querySelector("#grid");
+    const listButton = document.querySelector("#list");
+    const display = document.querySelector("article");
+
+    gridButton.addEventListener("click", () => {
+        display.classList.add("grid");
+        display.classList.remove("list");
+    });
+
+    listButton.addEventListener("click", showList);
+
+    function showList() {
+        display.classList.add("list");
+        display.classList.remove("grid");
+    }
+
+    fetch("scripts/members.json")
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(member => {
+                const section = document.createElement("section");
+                section.innerHTML = `
+                    <img src="images/${member.image}" alt="${member.name}">
+                    <h3>${member.name}</h3>
+                    <p>${member.address}</p>
+                    <p>${member.phone}</p>
+                    <a href="${member.website}" target="_blank">Their Website</a>
+                    <p>Membership Level: ${member.membershipLevel}</p>
+                `;
+                display.appendChild(section);
+            });
+        });
+});
